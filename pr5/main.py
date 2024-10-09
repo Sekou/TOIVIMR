bc51d19f1cdcfb244b3acb9f0366547abb186b6a
Sekou <sekoudiane1990@gmail.com>
2024-10-09 04:06:03 +0300

    pr5 - FAST descriptor update

--- pr5/main.py
+++ pr5/main.py
@@ -13,28 +13,26 @@ deltas=[[0,-3], [1,-3], [2,-2], [3,-1], [3, 0], [3,1], [2,2],
 
 def getPx(img, x, y):
     if x<0 or y<0 or x>=img.shape[1] or y>=img.shape[0]:
-        return [0,0,0]
-    return img[y, x]
+        return 0
+    return np.sum(img[y, x])/3
 
 def getDescr(img, x, y):
     thr = 20
     descr=[]
-    px=getPx(img, x, y)
-    v=np.sum(px)/3
+    v=getPx(img, x, y)
     n1, n2, n3 = 0, 0, 0
     for d in deltas:
-        px2=getPx(img, x+d[0], y+d[1])
-        v2 = np.sum(px2) / 3
-        if v2>v+thr: #крайний пиксель светлее центра
-            n1+=1; n2, n3 = 0, 0
-            descr.append(0)
+        v2=getPx(img, x+d[0], y+d[1])        
         if v2<v-thr: #крайний пиксель темнее центра
-            n2+=1; n1, n3 = 0, 0
+            n1+=1; n2, n3 = 0, 0
             descr.append(1)
         if v+thr>v2>v-thr: #крайний пиксель похожий
-            n3+=1; n1, n2 = 0, 0
+            n2+=1; n1, n3 = 0, 0
             descr.append(2)
-    if max([n1, n2])>=12:
+        if v2>v+thr: #крайний пиксель светлее центра
+            n3+=1; n1, n2 = 0, 0
+            descr.append(0)
+    if max([n1, n3])>=12:
         return descr
     else:
         return None
@@ -42,7 +40,8 @@ def getDescr(img, x, y):
 def getFeatureMap(img):
     fm=np.zeros(img.shape)
     for iy in range(img.shape[0]):
-        print(iy)
+        progress=100*iy/img.shape[0]
+        print(f"Progress: {progress:.3f}%")
         for ix in range(img.shape[1]):
             fm[iy, ix]=[255, 255, 255] \
             if getDescr(img, ix, iy) is not None \
@@ -52,6 +51,4 @@ def getFeatureMap(img):
 fm=getFeatureMap(img)
 cv2.imwrite("fm.jpg", fm)
 cv2.imshow("fm", fm)
-
-
 cv2.waitKey(0)
