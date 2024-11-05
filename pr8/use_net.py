21c9cd3ead37f92d3cea03067a31852ff68c4b8f
Sekou <sekoudiane1990@gmail.com>
2024-11-05 19:21:20 +0300

    pr8 - neural detection of X and O - centering

--- pr8/use_net.py
+++ pr8/use_net.py
@@ -13,6 +13,20 @@ arr = np.asarray(scene)
 sz = network.sz
 step = sz//5
 
+def getCenter(img):
+    mx, my, sx, sy=0,0,0,0
+    for iy in range(0, img.shape[0]):
+        for ix in range(0, img.shape[1]):
+            v=255 - sum(img[iy,ix])/3
+            mx+=v*ix
+            my+=v*iy
+            sx+=v
+            sy+=v
+    mx/=sx
+    my/=sy
+    return [mx, my]
+
+
 for iy in range(0, arr.shape[0]-sz, step):
     print(f"y={iy} of {arr.shape[0]-sz}")
     for ix in range(0, arr.shape[1]-sz, step):
@@ -20,7 +34,11 @@ for iy in range(0, arr.shape[0]-sz, step):
         input=np.reshape(input, (1, sz, sz, network.nchannels))
         predictions = net.predict(input)[0]
         ind=np.argmax(predictions)
-        if ind<2 and predictions[ind]>0.99 and predictions[2]<0.5:
+        cx, cy=getCenter(input[0])
+        #для центровки образа
+        d=max(abs(cx-input.shape[1]//2), abs(cy-input.shape[1]//2))
+
+        if ind<2 and predictions[ind]>0.99 and predictions[2]<0.5 and d<3:
             color=(255,0,0) if ind==0 else (0,255,0)
             cv2.rectangle(scene, (ix, iy), (ix+sz, iy+sz), color, 1)
 
